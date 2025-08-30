45a236beb60f82e44bc31770bee81f5a411bd3b2
pkunray <pkunray@gmail.com>
2025-08-30 16:11:35 +0200

    feat: add max_sublist_sum function

---
+++ python_programs/max_sublist_sum.py
@@ -0,0 +1,21 @@
+def max_sublist_sum(arr):
+    max_ending_here = 0
+    max_so_far = 0
+
+    for x in arr:
+        max_ending_here = max_ending_here + x
+        max_so_far = max(max_so_far, max_ending_here)
+
+    return max_so_far
+
+"""
+Input:
+    arr: A list of ints
+
+Output:
+    The maximum sublist sum
+
+Example:
+    >>> max_sublist_sum([4, -5, 2, 1, -1, 3])
+    5
+"""
